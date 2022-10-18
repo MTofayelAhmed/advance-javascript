06e040e6afc67ed75403f23c828ddb8578c1efef
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-18 20:13:25 +0600

    true- false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+// const age = -4;
+// if (age){
+//   console.log("condition is true");
+// }
+// else{
+//  console.log( "condition is false");
+// }
+
+// const Name = "0 ";
+// if (Name){
+//   console.log("condition is true");
+// }
+// else{
+//  console.log( "condition is false");
+// }
+
+
+let Name;
+console.log(Name);
+if (Name){
+  console.log("condition is true");
+}
+else{
+ console.log( "condition is false");
+};
