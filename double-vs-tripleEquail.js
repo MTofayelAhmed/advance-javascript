73cc878f19ef0c1c2acc957aa4deab887a11f116
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-18 21:11:04 +0600

    double vs triple

---
+++ double-vs-tripleEquail.js
@@ -0,0 +1,44 @@
+// const first = 3; 
+// const second = 4;
+// if (first == second){
+//   console.log("condition is true");
+// }
+// else{console.log("condition is false")
+// };
+
+
+// const first = 3; 
+// const second = "3";
+// if (first == second){
+//   console.log("condition is true");
+// }
+// else{console.log("condition is false")
+// };
+
+
+// const first = 3; 
+// const second = "3";
+// if (first === second){
+//   console.log("condition is true");
+// }
+// else{console.log("condition is false")
+// };
+
+
+// double equal only check value not type on the other hand triple equal check both.
+
+// const first = 1; 
+// const second = true;
+// if (first == second){
+//   console.log("condition is true");
+// }
+// else{console.log("condition is false")
+// };
+
+const first = 0; 
+const second = false;
+if (first == second){
+  console.log("condition is true");
+}
+else{console.log("condition is false")
+};
