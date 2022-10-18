f4ce39bfc967e60fe1a4988f39152ab2dc972883
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-18 20:49:41 +0600

    learned javascript

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+// undefined
+// let pakhi;
+// console.log(pakhi);
+
+// function Add (num1, num2){
+//   console.log(num1 + num2);
+// }
+// const result = Add(14, 15);
+// console.log(result);
+
+// function addition(num1, num2){
+//   console.log(num1, num2);
+// };
+// const sum = addition(13);
+// console.log(sum);
+
+
+// const Premik = {id: 21, Name: "Rakib"};
+// console.log(Premik.gf);
+
+const Fun = undefined;
+console.log(Fun);
