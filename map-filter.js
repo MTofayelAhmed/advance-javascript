9f29b8a6080efab6e92666b8813c08b62e56456b
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-18 23:51:00 +0600

    object-property

--- map-filter.js
+++ map-filter.js
@@ -1,37 +1,37 @@
-const number = [2, 3, 4, 5, 6, 7, 8];
-// const output = [];
-// for(let i = 0; i < number.length; i++){
-//   const Element = number[i];
-//   const result = Element * Element;
-//   output.push(result);
-// }
+const number = [2, 3, 4, 5, 6, 7, 8]; 
+const output = [];
+for(let i = 0; i < number.length; i++){
+  const Element = number[i];
+  const result = Element * Element;
+  output.push(result);
+}
 
 
-// console.log(output);
+console.log(output);
 
 
 
-//  const result = number.map(function(element){
-//   return element * element;
-// })
-// console.log(result);
+ const result = number.map(function(element){
+  return element * element;
+})
+console.log(result);
 
 
-// function square(element){
-//   return element * element;
-// };
+function square(element){
+  return element * element;
+};
 
 
-// const result = square (12);
-// console.log(result);
+const result = square (12);
+console.log(result);
 
-// const result = element => element * element;
+const result = element => element * element;
 
-// const result = number.map(x=> x * x);
-// console.log(result);
+const result = number.map(x=> x * x);
+console.log(result);
 
-// filter concept 
-// const biggerNumber = number.filter(x => x>5);
-// console.log(biggerNumber);
+filter concept 
+const biggerNumber = number.filter(x => x>5);
+console.log(biggerNumber);
 const Bigger = number.find(x=> x>7);
 console.log(Bigger);
---
+++ object-property.js
@@ -0,0 +1,24 @@
+const students =
+[
+  {id: 21, Name: "Sakib Khan"},
+  {id: 11, Name: "Rakib Khan"},
+  {id: 31, Name: "Bakib Khan"},
+
+];
+// const output = [];
+
+// for (let i = 0; i<students.length; i++){
+//   const element = students[i];
+//   const studentName = element.Name;
+//   output.push(studentName);
+
+// };
+// console.log(output);
+
+const nameArray = students.map(x => x.Name);
+console.log(nameArray);
+const studentIDS = students.map(x=> x.id);
+console.log(studentIDS);
+const bigger = students.filter( x => x.id>20 );
+console.log(bigger);
+const biggerOne = students.find(x=> x.id > 30 );
