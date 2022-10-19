c5b46f800ad1ff15be398d543e69277ff9b3bf7a
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-19 21:52:49 +0600

    slice and splice

---
+++ array-split.js
@@ -0,0 +1,38 @@
+// const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+// const parts = numbers.slice(2, 5);
+// console.log(parts);
+// console.log(numbers);
+
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+const parts = numbers.splice(2,3, 66, 77);
+console.log(parts);
+
+
+
+console.log(parts);
+console.log(numbers);
+const together =numbers.join("");      
+console.log(together);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
