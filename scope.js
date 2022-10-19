468e76ebec94735106536f4141a0bbb53d6894ed
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-19 18:00:54 +0600

    socpe related discussion

---
+++ scope.js
@@ -0,0 +1,40 @@
+// if any element situated at outside of the function . it considered as 
+// a global element . anyone can call it from inside of a function and outside of a 
+// function
+
+
+// function sum (first, second){
+//   const total = first + second;
+//   console.log(total);
+//   return total;
+
+// }
+// const result = sum (7, 3);
+// // console.log(total); error khaba
+// console.log(result);
+
+
+
+
+// let babyAge =  12; 
+// function sum (first, second){
+//   const total = first + second + babyAge;
+//   console.log(total);
+//   console.log(babyAge);
+//   return total;
+// };
+// console.log(babyAge);
+// const result = sum (7, 3);
+// console.log(result);
+
+
+function sum (first, second){
+  const total = first + second;
+  if(total>9){
+    const mood = "happy";
+    console.log(mood);
+  }
+  return total;
+  console.log(mood);
+
+}
