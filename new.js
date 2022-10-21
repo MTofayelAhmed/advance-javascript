064896b42cde51292b4e5e4f964aea00f0d05642
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-10-21 20:40:23 +0600

    this

--- new.js
+++ new.js
@@ -14,10 +14,10 @@ console.log(friendlyPerson);
 
 // before es6, we used function instead of class
 
-function Person1(firstName, lastName, salary){
-  this.firstName= firstName;
-  this.lastName = lastName; 
-  this.salary = salary;
-};
-const oldPerson = new Person1("kalam", "samim", 50000);
-console.log(oldPerson);
+// function Person1(firstName, lastName, salary){
+//   this.firstName= firstName;
+//   this.lastName = lastName; 
+//   this.salary = salary;
+// };
+// const oldPerson = new Person1("kalam", "samim", 50000);
+// console.log(oldPerson);
